998d3f811854805af9499cf2589c20fe59d4ea21
Macbook <Macbook@La-Presisa.transoceanet.net>
2025-09-16 21:40:36 -0500

    fix: add homepage content

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body className="flex flex-col min-h-screen">{children}</body>
     </html>
   )
 }
